0af2b4901726751ae80622321c54dfb6e13f4135
Hao Wang <haowang@skight.ca>
2019-01-27 12:01:22 -0800

    Setup project from mvc template

---
+++ Invoice.UI/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Invoice.UI.Models;
+
+namespace Invoice.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Connect()
+        {
+           return Redirect("");
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "BW Invoice";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Invoice Module
+## Development Instruction
+1. To clone source code from github: git clone https://github.com/SkightTeam/BW.Invoice.git
+2. To set Inovice.UI as start up project if it is not yet.
+3.
